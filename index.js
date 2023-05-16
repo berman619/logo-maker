57ac6953b65206b639889def40ab6a9cdf285524
Zach Berger <zachhberger@gmail.com>
2023-05-15 20:37:44 -0400

    build inquirer prompt

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
-//Runs the application using imports from lib/
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Square, Circle, Triangle } = require('./lib/shapes');
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'shapeChoice',
+      message: 'Select a shape:',
+      choices: ['Triangle', 'Circle', 'Square'],
+    },
+    {
+      type: 'list',
+      name: 'textColor',
+      message: 'Select a text color:',
+      choices: ['Red', 'Green', 'Blue', 'Yellow', 'Custom'],
+    },
+    {
+      type: 'input',
+      name: 'customTextColor',
+      message: 'Enter a custom text color (hexadecimal value):',
+      //only display this question if previous answer is custom
+      when: (answers) => answers.textColor === 'Custom',
+    },
+    {
+      type: 'list',
+      name: 'shapeColor',
+      message: 'Select a shape color:',
+      choices: ['Red', 'Green', 'Blue', 'Yellow', 'Custom'],
+    },
+    {
+      type: 'input',
+      name: 'customShapeColor',
+      message: 'Enter a custom shape color (hexadecimal value):',
+      //only display this question if previous answer is custom
+      when: (answers) => answers.shapeColor === 'Custom',
+    },
+  ])
+  .then((answers) => {
+    const { shapeChoice, textColor, customTextColor, shapeColor, customShapeColor } = answers;
+
+    let shapeInstance;
+
+    if (shapeChoice === 'Triangle') {
+      shapeInstance = new Triangle();
+    } else if (shapeChoice === 'Circle') {
+      shapeInstance = new Circle();
+    } else if (shapeChoice === 'Square') {
+      shapeInstance = new Square();
+    }
+
+    const selectedTextColor = textColor === 'Custom' ? customTextColor : textColor;
+    const selectedShapeColor = shapeColor === 'Custom' ? customShapeColor : shapeColor;
+
+    shapeInstance.setColor(selectedShapeColor);
+
+    const svgString = shapeInstance.render();
+    
+  });
